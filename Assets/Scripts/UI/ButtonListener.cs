61a9e38c9c572d05c83036739f2438d86d455d9a
James Wills <jamesbwills@Jamess-MacBook-Pro.local>
2020-05-31 19:03:59 -0700

    add additional files that were missed in first PR

---
+++ Assets/Scripts/UI/ButtonListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonListener : MonoBehaviour
+    {
+        public ButtonType buttonType;
+        
+        private MenuActionHandler _menuActionHandler;
+
+        private Button _button;
+    
+        private void Awake()
+        {
+            _menuActionHandler = GameObject.FindGameObjectWithTag("Canvas").GetComponent<MenuActionHandler>();
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnDestroy() => _button.onClick.RemoveListener(OnButtonClick);
+        
+        private void OnButtonClick() => _menuActionHandler.OnButtonClicked(buttonType);
+    }
+}
---
+++ Assets/Scripts/UI/CanvasController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    /**
+     * This class is only used to add a type to each menu canvas so they're easily searchable.
+     */
+    public class CanvasController : MonoBehaviour
+    {
+        public CanvasType canvasType;
+    }
+}
